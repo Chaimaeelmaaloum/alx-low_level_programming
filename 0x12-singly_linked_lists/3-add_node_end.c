5177e2051ea122f4ab8d9d81651eb14678dcc3b6
chaimae <chaimae.elmaaloum57@gmail.com>
2023-06-01 15:32:24 -0700

    tete

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,31 +12,31 @@
  */
 list_t *a_n_e(list_t **head, const char *str)
 {
-	list_t *n;
+	list_t *new;
 	list_t *temp = *head;
 	unsigned int len = 0;
 
 	while (str[len])
 		len++;
 
-	n = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
 	if (!n)
 		return (NULL);
 
-	n->str = strdup(sr);
-	n->len = len;
-	n->next = NULL;
+	new->str = strdup(sr);
+	new->len = len;
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = n;
-		return (n);
+		*head = new;
+		return (new);
 	}
 
 	while (temp->next)
 		temp = temp->next;
 
-	temp->next = n;
+	temp->next = new;
 
-	return (n);
+	return (new);
 }
