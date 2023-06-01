ffcca12218b2d932f11d5dfe2f949b96f814f61f
chaimae <chaimae.elmaaloum57@gmail.com>
2023-06-01 13:19:05 -0700

    tast

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,10 +13,12 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!h->structure)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->structure);
+			printf("[%u] %s\n");
+			printf("h->len");
+	       		printf("h->str");
 		h = h->next;
 		s++;
 	}
