a8cdc4ef42236fa87ede67b75c8f5b2dde615eec
chaimae <chaimae.elmaaloum57@gmail.com>
2023-06-01 15:03:48 -0700

    tattsk

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,24 +6,24 @@
 /**
  * add_node - we will add a n node in the beginning of the linked list
  *
- * @str: n string will be added in the node.
+ * @string: n string will be added in the node.
  * @h: we have two pointer at the list_t
  * Return: will be the address of the n element, or NULL if it fails
  */
 
-list_t *add_node(list_t **h, const char *str)
+list_t *add_node(list_t **h, const char *string)
 {
 	list_t *n;
 	unsigned int len = 0;
 
-	while (str[len])
+	while (string[len])
 		len++;
 
 	n = malloc(sizeof(list_t));
 	if (!n)
 		return (NULL);
 
-	n->str = strdup(str);
+	n->string = strdup(string);
 	n->len = len;
 	n->next = (*h);
 	(*h) = n;
