c9b9f8909b21798ba7b0d77140a14b086f578195
chaimae <chaimae.elmaaloum57@gmail.com>
2023-06-01 16:20:28 -0700

    function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
 void first(void)__attribute__ ((constructor)); 
-
 /**
  * first  print the function before the main
  * we will use printf
  * then excute the function
- */
+*/
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
