e6ba348f18440c25c797bedb87c55f2837f2a15b
chaimae <chaimae.elmaaloum57@gmail.com>
2023-06-01 13:30:51 -0700

    tasktwo

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
  * list_len 
  * @h: pointer to the list_t list
- *
+ * any
  * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
