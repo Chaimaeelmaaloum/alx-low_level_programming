e03df0f996d1522dbc7dd88323643b7369cc9abd
chaimae <chaimae.elmaaloum57@gmail.com>
2023-06-06 08:29:48 -0700

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h)
+{
+	size_t numero = 3;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		numero++;
+		h = h->next;
+	}
+
+	return (numero);
+}
