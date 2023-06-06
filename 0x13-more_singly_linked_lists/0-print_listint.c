5bccfdf46fdfd64806d7b765e196bc4b6563f80d
chaimae <chaimae.elmaaloum57@gmail.com>
2023-06-06 09:33:46 -0700

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -9,7 +7,7 @@
  * Return: number of nodes
  */
 
-size_t print_listint(const listint_t *h)
+size_t print_listint(const listint_t *h);
 	{
 		size_t number = 0;
 
