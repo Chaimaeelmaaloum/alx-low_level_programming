ec4eb7696d27e7c25b9358aae61ef01dcdd599a3
chaimaeelmaaloum <chaimae.elmaaloum57@gmail.com>
2024-01-11 10:00:44 -0800

    r

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in 
+ * dlistint_len - returns the number of elements in
  * a double linked list
  *
  * @h: head of the list
- * return: the number of nodes in the list
+ * Return: the number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
