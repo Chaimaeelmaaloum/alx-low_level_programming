7ed76248699aedbd8cd6a6771e124a7ac5970a32
chaimaeelmaaloum <chaimae.elmaaloum57@gmail.com>
2024-01-15 10:49:30 -0800

    h

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+/** 
+ * add - add two integers
+ * @a : first int
+ * @b : second int 
+ * Return : addition 
+ */
+int add (int a, int b)
+{
+	return (a+b);
+}
+#include <stdlib.h> 
+
+/** 
+ * add - add two integers
+ * @a : first int
+ * @b : second int 
+ * Return : substracted final result 
+ */
+int sub (int a, int b)
+{
+	return (a - b );
+}
+#include <stdio.h>
+/** 
+ * div - divide two integers
+ * @a : first int
+ * @b : second int 
+ * Return : devision result 
+ */
+int div (int a, int b)
+{
+	return (a / b);
+}
+#include <stdlib.h> 
+
+/** 
+ * add - add two integers
+ * @a : first int
+ * @b : second int 
+ * Return : substracted final result 
+ */
+int sub (int a, int b)
+{
+	return (a - b );
+}
+#include <stdio.h>
+#include <stdlib.h> 
+
+/** 
+ * mod - mod of two integers
+ * @a : first int
+ * @b : second int 
+ * Return : final answer 
+ */
+int mod (int a, int b)
+{
+	return (a % b);
+}
+#include <stdio.h>
+#include <stdlib.h> 
+
+/** 
+ * mul - multiply two integers
+ * @a : first int
+ * @b : second int 
+ * Return : multiply
+ */
+int add (int a, int b)
+{
+	return (a * b);
+}
