ab44a3f8397d0e6f6a5577f232f9ce3f550fd2c9
chaimaeelmaaloum <chaimae.elmaaloum57@gmail.com>
2024-01-19 04:52:37 -0800

    t

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-gcc -c *.c -fpic &&g gcc *o -shared liball.so
+#/!bin/bash
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c -fPIC *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
